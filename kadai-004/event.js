c94e4cd9230025b27e24e92fb52cb6ae3ac63ab6
shohei-saito07 <56618646+shohei-saito07@users.noreply.github.com>
2023-11-25 02:05:58 +0900

    jqueryの課題４

---
+++ kadai-004/event.js
@@ -0,0 +1,9 @@
+$(window).on('load', () => {
+    // ロードされた場合
+    console.log('loadイベントが発生しました');
+});
+
+$(window).on('scroll', () => {
+    // スクロールされた場合
+    console.log('scrollイベントが発生しました');
+});
